011e63ca88499704e51e43e25db003c1e026d181
CR <44606665+CodebyCR@users.noreply.github.com>
2022-12-25 00:48:38 +0100

    Add some functions to StringUtils.hpp

--- StringUtils.hpp
+++ StringUtils.hpp
@@ -29,5 +29,45 @@ namespace StringUtils{
         return result;
     }
 
+
+    /**
+     * returns a string as vector of strings, split by the delimiter characters
+     * @param str
+     * @param delimiters
+     * @return
+     */
+    auto split_by_any_of( std::string& str , std::string_view const& delimiters) -> std::vector<std::string> {
+        std::vector<std::string> result;
+
+        for(auto delimiter : delimiters) {
+            auto split = split_by(str, delimiter);
+
+            auto currentDelimiter = std::string(1, delimiter);
+                result.push_back( split.at(0));
+                result.push_back( currentDelimiter);
+
+            if(split.size() > 1) {
+                str = split.at(1);
+            }
+        }
+
+        result.push_back(str);
+
+
+        return result;
+    }
+
+    auto contains(std::string_view const& str, std::string_view const& substring) -> bool {
+        return str.find(substring) != std::string::npos;
+    }
+
+    auto is_number(std::string_view const& str) -> bool {
+        return !str.empty() && std::find_if(str.begin(), str.end(), [](char c) { return !std::isdigit(c); }) == str.end();
+    }
+
+    auto count_occurrences(std::string_view const& str, char const& character) -> uint8_t {
+        return std::count(str.begin(), str.end(), character);
+    }
+
 }
 
