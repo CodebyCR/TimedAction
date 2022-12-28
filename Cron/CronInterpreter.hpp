556bd6f85b2e6a6de89fdd81cf244691513ed197
CR <44606665+CodebyCR@users.noreply.github.com>
2022-12-29 00:44:44 +0100

    Create to_date_time & print function

--- Cron/CronInterpreter.hpp
+++ Cron/CronInterpreter.hpp
@@ -5,34 +5,36 @@
 #pragma once
 #include <iostream>
 #include <Cron.hpp>
+#include <iomanip>
 
 namespace CronInterpreter {
 
     auto cartesian_product(Cron const& cronObject)
     {
-        const std::vector<u_long> secondTimes = cronObject.getSecondTimes();
-        const std::vector<u_long> minuteTimes = cronObject.getMinuteTimes();
-        const std::vector<u_long> hourTimes = cronObject.getHourTimes();
-        const std::vector<u_long> dayOfMonthTimes = cronObject.getDayOfMonthTimes();
-        const std::vector<u_long> monthTimes = cronObject.getMonthTimes();
+        const std::forward_list<std::chrono::seconds> secondTimes = cronObject.getSecondTimes();
+        const std::forward_list<std::chrono::seconds> minuteTimes = cronObject.getMinuteTimes();
+        const std::forward_list<std::chrono::seconds> hourTimes = cronObject.getHourTimes();
+        const std::forward_list<std::chrono::seconds> dayOfMonthTimes = cronObject.getDayOfMonthTimes();
+        const std::forward_list<std::chrono::seconds> monthTimes = cronObject.getMonthTimes();
         //const std::vector<u_long> weekDayTimes = cronObject.getWeekDayTimes(); has filter function
-        const std::vector<u_long> yearTimes = cronObject.getYearTimes();
+        const std::forward_list<std::chrono::seconds> yearTimes = cronObject.getYearTimes();
 
-        std::vector<std::vector<u_long>> result;
+        std::vector<std::vector<std::chrono::seconds>> result;
 
-        for (const auto& x : secondTimes)
-        {
-            for (const auto& y : minuteTimes)
-            {
-                for (const auto& z : hourTimes)
-                {
-                    for (const auto& w : dayOfMonthTimes)
-                    {
-                        for (const auto& u : monthTimes)
-                        {
-                            for (const auto& t : yearTimes)
-                            {
-                                result.emplace_back(std::vector<u_long>{x, y, z, w, u, t});
+        for (auto &second : secondTimes) {
+            for (auto &minute : minuteTimes) {
+                for (auto &hour : hourTimes) {
+                    for (auto &dayOfMonth : dayOfMonthTimes) {
+                        for (auto &month : monthTimes) {
+                            for (auto &year : yearTimes) {
+                                std::vector<std::chrono::seconds> timeVector;
+                                timeVector.push_back(second);
+                                timeVector.push_back(minute);
+                                timeVector.push_back(hour);
+                                timeVector.push_back(dayOfMonth);
+                                timeVector.push_back(month);
+                                timeVector.push_back(year);
+                                result.push_back(timeVector);
                             }
                         }
                     }
@@ -40,6 +42,76 @@ namespace CronInterpreter {
             }
         }
 
+
         return result;
     }
+
+    auto to_date_time(std::vector<std::chrono::seconds> const& timeVector)
+    {
+        std::tm tm = {};
+        tm.tm_sec = timeVector[0].count();
+        tm.tm_min = timeVector[1].count();
+        tm.tm_hour = timeVector[2].count();
+        tm.tm_mday = timeVector[3].count();
+        tm.tm_mon = timeVector[4].count();
+        tm.tm_year = timeVector[5].count();
+        tm.tm_isdst = -1;
+        return std::chrono::system_clock::from_time_t(std::mktime(&tm));
+    }
+
+    auto to_date_time(std::vector<std::vector<std::chrono::seconds>> const& timeVector)
+    {
+        std::vector<std::chrono::system_clock::time_point> result;
+        for (const auto& x : timeVector)
+        {
+            result.emplace_back(to_date_time(x));
+        }
+        return result;
+    }
+
+    auto filter(Cron const& cronObject, std::vector<std::chrono::system_clock::time_point> const& timePoints)
+    {
+        std::vector<std::vector<std::chrono::seconds>> result;
+
+        // filter out all invalid timePoints dates
+//        for (const auto& x : timePoints)
+//        {
+//            std::tm tm = *std::localtime(&std::chrono::system_clock::to_time_t(x));
+//            if (cronObject.getWeekDayTimes().contains(tm.tm_wday))
+//            {
+//                result.emplace_back(std::vector<std::chrono::seconds>{std::chrono::seconds(tm.tm_sec),
+//                                                                      std::chrono::seconds(tm.tm_min),
+//                                                                      std::chrono::seconds(tm.tm_hour),
+//                                                                      std::chrono::seconds(tm.tm_mday),
+//                                                                      std::chrono::seconds(tm.tm_mon),
+//                                                                      std::chrono::seconds(tm.tm_year)});
+//            }
+//        }
+
+        return result;
+    }
+
+
+
+    static auto print_time_points(Cron const& cronObject){
+        auto timeMatrix = cartesian_product(cronObject);
+        auto timePoints = to_date_time(timeMatrix);
+
+        // timePoint vector to local time
+        std::vector<std::tm> localTimePoints;
+        for (const auto& x : timePoints)
+        {
+            auto time = std::chrono::system_clock::to_time_t(x);
+            localTimePoints.emplace_back(*std::localtime(&time));
+        }
+
+        // print timePoints
+        std::cout << " TimePoints size: " << timePoints.size() << std::endl;
+        std::cout << " Local TimePoints size: " << localTimePoints.size() << std::endl;
+        for (const auto& x : localTimePoints)
+        {
+            std::cout << std::put_time(&x, "%c") << std::endl;
+        }
+    }
+
 }
