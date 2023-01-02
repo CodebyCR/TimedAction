34c5e1322e80d076447c510acd0e0467fc1547f9
CR <44606665+CodebyCR@users.noreply.github.com>
2023-01-02 16:00:39 +0100

    Fix CronPart count

--- Cron/CronRegex.hpp
+++ Cron/CronRegex.hpp
@@ -12,70 +12,81 @@
 
 namespace CronRegex {
 
-    const auto SECOND_REGEX = std::regex(R"(^\s*(\*|[0-5]?\d)(/[0-5]?\d)?\s+)");
-    const auto MINUTE_REGEX  = std::regex(R"(^\s*(\*|[0-5]?\d)(/[0-5]?\d)?\s+)");
-    const auto HOUR_REGEX  = std::regex(R"(^\s*(\*|[0-5]?\d)(/[0-5]?\d)?\s+)");
-    const auto DAY_OF_MONTH_REGEX  = std::regex(R"(^\s*(\*|[0-5]?\d)(/[0-5]?\d)?\s+)");
-    const auto MONTH_REGEX  = std::regex(R"(^\s*(\*|[0-5]?\d)(/[0-5]?\d)?\s+)");
+//    const std::string VALID_SECOND = R"(^([0-5]?[0-9]|\*)(-([0-5]?[0-9]))?(/([0-9]))?(,([0-5]?[0-9])(-([0-5]?[0-9]))?(/([0-9]))?)*$)";
+//    const auto SECOND_REGEX = std::regex(VALID_SECOND);
+//
+//    const std::string  VALID_MINUTE = R"(^([0-5]?[0-9]|\*)(-([0-5]?[0-9]))?(/([0-9]))?(,([0-5]?[0-9])(-([0-5]?[0-9]))?(/([0-9]))?)*$)";
+//    const auto MINUTE_REGEX = std::regex(VALID_MINUTE);
+//
+//    const std::string  VALID_HOUR = R"(^((\*)|(((([0-1]?[0-9])|([2][0-3])|([0-9]))(-(([0-1]?[0-9])|([2][0-3])|([0-9])))?)(,((([0-1]?[0-9])|([2][0-3])|([0-9]))(-(([0-1]?[0-9])|([2][0-3])|([0-9])))?))*)))$)";
+//    const auto HOUR_REGEX = std::regex(VALID_HOUR);
+//
+//    const std::string  VALID_DAY_OF_MONTH = R"(^((\*)|(((([1-2]?[0-9])|([3][0-1])|([0-9]))(-(([1-2]?[0-9])|([3][0-1])|([0-9])))?)(,((([1-2]?[0-9])|([3][0-1])|([0-9]))(-(([1-2]?[0-9])|([3][0-1])|([0-9])))?))*)))$)";
+//    // R"(^([1-9]|[12][0-9]|3[01]|\*)(-([1-9]|[12][0-9]|3[01]))?(/([1-9][0-9]*))?(,([1-9]|[12][0-9]|3[01])(-([1-9]|[12][0-9]|3[01]))?(/([1-9][0-9]*))?)*$)"
+//    const auto DAY_OF_MONTH_REGEX = std::regex(VALID_DAY_OF_MONTH);
+//
+//    const std::string  VALID_MONTH = R"(^((\*)|(((([1-9])|([1][0-2])|([0-9]))(-(([1-9])|([1][0-2])|([0-9])))?)(,((([1-9])|([1][0-2])|([0-9]))(-(([1-9])|([1][0-2])|([0-9])))?))*)))$)";
+//    // R"(^((\*)|(((([1-9])|([1][0-2]))(-(([1-9])|([1][0-2])))?)(,((([1-9])|([1][0-2]))(-(([1-9])|([1][0-2])))?))*)))$)")
+//    const auto MONTH_REGEX = std::regex(VALID_MONTH);
+//
+//    const std::string  VALID_DAY_OF_WEEK = R"(^((\*)|(((([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT))(-(([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT)))?)(,((([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT))(-(([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT)))?))*)))$)";
+//    const auto DAY_OF_WEEK_REGEX = std::regex(VALID_DAY_OF_WEEK);
+//
+//    const std::string  VALID_YEAR = R"(^((\*)|(((([1-9][0-9][0-9][0-9])|([0-9][0-9][0-9][0-9]))(-(([1-9][0-9][0-9][0-9])|([0-9][0-9][0-9][0-9])))?)(,((([1-9][0-9][0-9][0-9])|([0-9][0-9][0-9][0-9]))(-(([1-9][0-9][0-9][0-9])|([0-9][0-9][0-9][0-9])))?))*)))$)";
+//    // R"(^((\*)|(((([1970-2099])|(\*\*))(-(([1970-2099])|(\*\*)))?)(,((([1970-2099])|(\*\*))(-(([1970-2099])|(\*\*)))?))*)))$)"
+//    const auto YEAR_REGEX = std::regex(VALID_YEAR);
 
-    const auto DAY_OF_WEEK_REGEX  = std::regex(R"(^((\*)|(((([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT))(-(([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT)))?)(,((([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT))(-(([0-6])|(SUN|MON|TUE|WED|THU|FRI|SAT)))?))*)))$)");
-    const auto YEAR_REGEX  = std::regex(R"(^((\*)|(((([1970-2099])|(\*\*))(-(([1970-2099])|(\*\*)))?)(,((([1970-2099])|(\*\*))(-(([1970-2099])|(\*\*)))?))*)))$)");
-
-    auto split(const std::string& str, char delimiter) -> std::vector<std::string> {
-        std::vector<std::string> res;
-        std::stringstream ss(str);
-        std::string item;
-        while (std::getline(ss, item, delimiter)) {
-            res.push_back(item);
-        }
-        return res;
-    }
 
     [[maybe_unused]]
-    static auto isCronRegex(const std::string& cronString) {
+    static auto isValidCron(std::vector<std::string> &cronParts) -> bool {
 
-        if(cronString.empty()) {
+        if (cronParts.size() < 6) {
+            std::cerr << "Cron expression must have at least 6 parts" << std::endl;
             return false;
         }
 
-        auto cronParts = split(cronString, ' ');
-
-        if(cronParts.size() < 5) {
-            return false;
-        }
-
-        if (cronParts.size() == 5){
+        if (cronParts.size() == 6) {
             // without seconds
             cronParts.emplace(cronParts.begin(), "0");
         }
 
 
-        if(constexpr auto SECOND = 0; std::regex_match(cronParts.at(SECOND), SECOND_REGEX )) {
-            return true;
-        }
+//        if (constexpr auto SECOND = 0; !std::regex_match(cronParts.at(SECOND), SECOND_REGEX)) {
+//            std::cerr << "Invalid second: " << cronParts.at(SECOND) << std::endl;
+//            return false;
+//        }
+//
+//        if (constexpr auto MINUTE = 1; !std::regex_match(cronParts.at(MINUTE), MINUTE_REGEX)) {
+//            std::cerr << "Invalid minute: " << cronParts.at(MINUTE) << std::endl;
+//            return false;
+//        }
+//
+//        if (constexpr auto HOUR = 2; !std::regex_match(cronParts.at(HOUR), HOUR_REGEX)) {
+//            std::cerr << "Invalid hour: " << cronParts.at(HOUR) << std::endl;
+//            return false;
+//        }
+//
+//        if (constexpr auto DAY_OF_MONTH = 3; !std::regex_match(cronParts.at(DAY_OF_MONTH), DAY_OF_MONTH_REGEX)) {
+//            std::cerr << "Invalid day of month: " << cronParts.at(DAY_OF_MONTH) << std::endl;
+//            return false;
+//        }
+//
+//        if (constexpr auto MONTH = 4; !std::regex_match(cronParts.at(MONTH), MONTH_REGEX)) {
+//            std::cerr << "Invalid month: " << cronParts.at(MONTH) << std::endl;
+//            return false;
+//        }
+//
+//        if (constexpr auto DAY_OF_WEEK = 5; !std::regex_match(cronParts.at(DAY_OF_WEEK), DAY_OF_WEEK_REGEX)) {
+//            std::cerr << "Invalid day of week: " << cronParts.at(DAY_OF_WEEK) << std::endl;
+//            return false;
+//        }
+//
+//        if (constexpr auto YEAR = 6; !std::regex_match(cronParts.at(YEAR), YEAR_REGEX)) {
+//            std::cerr << "Invalid year: " << cronParts.at(YEAR) << std::endl;
+//            return false;
+//        }
 
-        if(constexpr auto MINUTE = 1; std::regex_match(cronParts.at(MINUTE), MINUTE_REGEX )) {
-            return true;
-        }
-
-        if(constexpr auto HOUR = 2; std::regex_match(cronParts.at(HOUR), HOUR_REGEX )) {
-            return true;
-        }
-
-        if(constexpr auto DAY_OF_MONTH = 3; std::regex_match(cronParts.at(DAY_OF_MONTH), DAY_OF_MONTH_REGEX )){
-            return true;
-        }
-
-        if(constexpr auto MONTH = 4; std::regex_match(cronParts.at(MONTH), MONTH_REGEX )) {
-            return true;
-        }
-
-        if(constexpr auto DAY_OF_WEEK = 5; std::regex_match(cronParts.at(DAY_OF_WEEK), DAY_OF_WEEK_REGEX )) {
-            return true;
-        }
-
-
-
+        return true;
     }
 
 
