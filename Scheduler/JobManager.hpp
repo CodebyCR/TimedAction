bf775a6e811cd128cc659ca064cf97c87ba5fd03
CR <44606665+CodebyCR@users.noreply.github.com>
2023-04-22 01:25:34 +0200

    Add JobManager.hpp

---
+++ Scheduler/JobManager.hpp
@@ -0,0 +1,22 @@
+//
+// Created by Christoph Rohde on 21.04.23.
+//
+
+#pragma once
+
+#include <iostream>
+
+/// @brief The JobManager class is responsible for managing the jobs.
+/// error handling, logging, etc.
+/// the Job Manager contains a reference to the EventQueue to be thread safe
+class JobManager {
+private:
+
+
+public:
+
+
+};
+
+
+
