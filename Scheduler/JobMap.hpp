35d81d7dae55ca6af524fbc37ebb75851e912860
CR <44606665+CodebyCR@users.noreply.github.com>
2023-04-23 22:13:23 +0200

    Add sort & get function

--- Scheduler/JobMap.hpp
+++ Scheduler/JobMap.hpp
@@ -5,11 +5,12 @@
 #pragma once
 
 #include <iostream>
-#include <map>
-#include <sort>
+#include <vector>
+#include <algorithm>
+#include <ranges>
 #include "../TimedAction_Types/I_TimedAction.hpp"
 
-class JobMap : public std::vector<std::pair<std::tm, I_TimedAction*>> {
+class JobMap: public std::vector<std::pair<std::time_t, I_TimedAction*>> {
 private:
 
 public:
@@ -19,22 +20,50 @@ public:
     JobMap &operator=(const JobMap &) = default;
 
 
+    /** sorts this vector by the key of the pair entries */
+    auto sort() -> void {
+        std::sort(this->begin(), this->end(), [](auto &left, auto &right) {
+            return &left.first < &right.first;
+        });
+    }
+
     auto put(std::shared_ptr<I_TimedAction> const& action) -> void {
 
         auto executionTimes = action->get_execution_times();
 
         for (auto & executionTime : executionTimes) {
             std::cout << "JobMap: put " << action->getName() << " for execution at " << std::asctime(&executionTime) << std::endl;
-            this->push_back(std::make_pair(executionTime, action.get()));
+
+            const auto time_t = std::mktime(&executionTime);
+            this->push_back(std::make_pair(time_t, action.get()));
         }
 
+        this->sort();
     }
 
-    auto sort() -> void {
-        std::sort(this->begin(), this->end(), [](auto const& a, auto const& b) {
-            return a.first < b.first;
+    /** return all actions that are scheduled for execution at the given time */
+    auto get(std::time_t const& time) -> std::vector<I_TimedAction*> {
+        std::vector<I_TimedAction*> result;
+
+        auto result_vec = std::ranges::filter_view(*this, [&](auto &pair) {
+            return pair.first == time;
         });
+
+        for (auto &pair : result_vec) {
+            result.emplace_back(pair.second);
+        }
+
+        return result;
     }
 
+    auto remove(std::time_t const& time) -> void {
+        auto it = std::find_if(this->begin(), this->end(), [&](auto &pair) {
+            return pair.first == time;
+        });
+
+        if (it != this->end()) {
+            this->erase(it);
+        }
+    }
 
 };
