cbdac45d12f593732f807c6f2f7f22d5917d1d07
Chris <christoph_rohde@icloud.com>
2023-06-01 21:56:30 +0200

    Add I_ExecutionTimer.hpp Interface

---
+++ Interfaces/I_ExecutionTimer.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Chris on 01.06.2023.
+//
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+/// <br/><b>I_ExecutionTimer</b><br/>
+/// Can used by the TimedAction Object to get the execution times.
+class I_ExecutionTimer {
+    [[nodiscard]] virtual auto get_execution_times() -> std::vector<std::tm> = 0;
+};
+
+// * for cron, interval and time vector
