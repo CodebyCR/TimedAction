efffa6619fda8a62298bfa5697fe26d930f26216
Chris <christoph_rohde@icloud.com>
2023-06-01 21:58:32 +0200

    Add IntervalExecution.hpp

---
+++ ExecutionTimer/IntervalExecution.hpp
@@ -0,0 +1,27 @@
+//
+// Created by Chris on 01.06.2023.
+//
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include "../Interfaces/I_ExecutionTimer.hpp"
+
+class IntervalExecution : public std::chrono::milliseconds, I_ExecutionTimer {
+public:
+    IntervalExecution() = default;
+
+    IntervalExecution(std::chrono::milliseconds &interval) {
+        this->operator=(interval);
+    }
+
+    IntervalExecution(std::chrono::milliseconds &&interval) {
+        this->operator=(interval);
+    }
+
+    [[nodiscard]] auto get_execution_times() -> std::vector<std::tm> override {
+        std::cout << "NOT IMPLEMENTED YET!" << std::endl;
+        return std::vector<std::tm>();
+    }
+};
