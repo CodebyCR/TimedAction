917e2806bf8cee3695521ded967e1f8f0150ed7a
CR <44606665+CodebyCR@users.noreply.github.com>
2023-06-09 22:49:30 +0200

    Add Ranges Regex

---
+++ Cron/CronDebugger/src/CronRange.hpp
@@ -0,0 +1,97 @@
+//
+// Created by Christoph Rohde on 09.06.23.
+//
+
+#pragma once
+
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+namespace CronRange {
+
+    auto split(const std::string& str, char delimiter) -> std::vector<std::string> {
+        auto result = std::vector<std::string>();
+        auto current = std::string();
+        for (auto c : str) {
+            if (c == delimiter) {
+                result.push_back(current);
+                current.clear();
+            } else {
+                current += c;
+            }
+        }
+        result.push_back(current);
+        return result;
+    }
+
+
+    struct CronRange {
+        unsigned short start;
+        unsigned short end;
+
+        CronRange(const std::string& range) {
+            auto rangeValues = split(range, '-');
+            this->start = std::stoi(rangeValues[0]);
+            this->end = std::stoi(rangeValues[1]);
+        }
+    };
+
+    auto static range_matched(const std::string& range, const std::regex& regex ) -> bool {
+        if(!range.contains('-')){
+            return false;
+        }
+
+        if (CronRange rangeValues(range); rangeValues.start >= rangeValues.end) {
+            return false;
+        }
+
+        return std::regex_match(range, regex);
+    }
+
+    auto static second_range_matched(const std::string& range) -> bool {
+        const static std::string second_range = "^(0?[0-9]|[1-5][0-9])(-?(?!0)(0?[0-9]|[1-5][0-9]))?$";
+        std::regex second_range_regex(second_range);
+
+        if(!range.contains('-')){
+            return false;
+        }
+
+        if (CronRange rangeValues(range); rangeValues.start >= rangeValues.end) {
+            return false;
+        }
+
+        return std::regex_match(range, second_range_regex);
+    }
+
+    auto static hour_range_matched(const std::string& range) -> bool {
+        const static std::string hour_range = "^(0?[0-9]|1[0-9]|2[0-3])(-?(?!0)(0?[0-9]|1[0-9]|2[0-3]))?$";
+        std::regex hour_range_regex(hour_range);
+
+        if(!range.contains('-')){
+            return false;
+        }
+
+        if (CronRange rangeValues(range); rangeValues.start >= rangeValues.end) {
+            return false;
+        }
+
+        return std::regex_match(range, hour_range_regex);
+    }
+
+    auto day_range_matched(const std::string& range) -> bool {
+        const static std::string day_range = "^(0?[1-9]|[12][0-9]|3[01])(-?(?!0)(0?[1-9]|[12][0-9]|3[01]))?$";
+        std::regex day_range_regex(day_range);
+
+        if(!range.contains('-')){
+            return false;
+        }
+
+        if (CronRange rangeValues(range); rangeValues.start >= rangeValues.end) {
+            return false;
+        }
+
+        return std::regex_match(range, day_range_regex);
+    }
+}
