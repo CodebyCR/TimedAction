0cd6774f11b9d3505a2691ba3ea0c9e8e797aaa8
CR <44606665+CodebyCR@users.noreply.github.com>
2023-06-22 18:43:42 +0200

    Destruct JSONParser.hpp

--- Utilities/JSONParser.hpp
+++ Utilities/JSONParser.hpp
@@ -18,6 +18,7 @@
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <variant>
 
 
 /// This namespace ist for the parsing of JSON config files.
@@ -38,43 +39,15 @@ namespace JSONParser {
         return buffer.str();
     }
 
-    static auto parse_json_string(std::string& jsonString) -> std::map<std::string, std::string> {
-        // Remove whitespaces and newlines from the JSON string
-        jsonString.erase(std::remove_if(jsonString.begin(), jsonString.end(),
-                                        [](unsigned char c) { return std::isspace(c); }),
-                         jsonString.end());
-
-        std::map<std::string, std::string> translationMap;
-
-        // get pair
-        for(int index = 0; index < jsonString.length(); index++) {
-            if(jsonString[index] == '{') {
-                jsonString.erase(index, 1);
-            }
-            if(jsonString[index] == '}') {
-                jsonString.erase(index, 1);
-            }
-        }
-
-        jsonString = StringUtils::eraseChar(jsonString, '"');
-
-        std::vector<std::string> pairs = StringUtils::split_by(jsonString, ',');
-        for(const auto& pair: pairs) {
-            std::vector<std::string> keyValue = StringUtils::split_by(pair, ':');
-            const auto currentPair = std::pair<std::string, std::string>(keyValue.at(0), keyValue.at(1));
-
-            translationMap.insert(currentPair);
-        }
-
-        return translationMap;
-    }
 
 
-    [[deprecated("use ConfigJSON instead")]]
-    static auto json_to_string_map(std::filesystem::path const& filename) -> std::map<std::string, std::string> {
-        std::string json_input = JSONParser::readJsonFile(filename);
-        return JSONParser::parse_json_string(json_input);
-    }
+
+
+//    [[deprecated("use ConfigJSON instead")]]
+//    static auto json_to_string_map(std::filesystem::path const& filename) -> std::map<std::string, std::string> {
+//        std::string json_input = JSONParser::readJsonFile(filename);
+//        return JSONParser::parse_string_map(json_input);
+//    }
 
     static auto map_to_json_string(std::map<std::string, std::string> const& stringMap) -> std::string {
         /// * refactor with stringstream
@@ -100,13 +73,13 @@ namespace JSONParser {
     }
 
     [[deprecated("use ConfigJSON instead")]]
-    static auto string_map_to_json(std::map<std::string, std::string> & stringMap) -> std::filesystem::path {
-        auto settings_json_path = std::filesystem::path("/Users/christoph_rohde/Example/test.json");
-                // std::filesystem::path(stringMap.at("Settings_JSON")); /// Update settings.json Path
+    static auto string_map_to_json(std::map<std::string, std::string>& stringMap) -> std::filesystem::path {
+        auto settings_json_path = std::filesystem::path("/Users/christoph_rohde/Example/Scheduler.json");
+        // std::filesystem::path(stringMap.at("Settings_JSON")); /// Update settings.json Path
         const auto json_string = map_to_json_string(stringMap);
 
         if(!exists(settings_json_path) || !is_regular_file(settings_json_path)) {
-            std::cout << "File does not exist"<< std::endl;
+            std::cout << "File does not exist" << std::endl;
         }
 
         stringMap.at("test2") = "new Value";
@@ -122,5 +95,6 @@ namespace JSONParser {
         std::cerr << "File can't be opened. Path: " << settings_json_path << std::endl;
         return "";
     }
+
 
 }    // namespace JSONParser
