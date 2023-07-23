f0a9eb9bb0a3ec90aec6db9aa0e8a2b4da30dca3
CR <44606665+CodebyCR@users.noreply.github.com>
2023-07-23 18:38:54 +0200

    Adds  - dispatcher  - Scheduler attributes
    Implement
     - restart methode
    
    Enhancements
     - avoid possible memory leak
     - avoid magic numbers
     - better logging

--- Scheduler/Scheduler.hpp
+++ Scheduler/Scheduler.hpp
@@ -6,6 +6,7 @@
 
 #include "../Cron/CronInterpreter.hpp"
 #include "../Interfaces/I_TimedAction.hpp"
+#include "../Utilities/Dispacher.hpp"
 #include "EventQueue.hpp"
 #include "JobManager.hpp"
 #include "TimeTable.hpp"
@@ -21,15 +22,20 @@
 class Scheduler {
 private:
     std::chrono::system_clock::time_point up_time = {};
-    //    std::shared_ptr<EventQueue> eventQueue_ptr;
-    std::map<std::string_view, std::string_view> attributes;
+    std::map<std::string, std::string> attributes;
     std::shared_ptr<TimeTable> timeTable_ptr;
-    Watcher watcher = Watcher();
+    Watcher watcher;
 
+    /// STATIC CONSTANTS
+    static constexpr int DAY_SECONDS = 86'400;
+    static constexpr int HOUR_SECONDS = 3'600;
+    static constexpr int MINUTE_SECONDS = 60;
 
     Scheduler() {
         up_time = std::chrono::system_clock::now();
-        attributes = {};
+        Dispatcher dispatcher;
+        attributes = dispatcher.get_scheduler_attributes();
+        watcher.set_attributes(dispatcher.get_watcher_attributes());
         timeTable_ptr = std::make_shared<TimeTable>();
         // drop folder support
         timeTable_subscribe_listener();
@@ -90,7 +96,6 @@ public:
     void operator=(Scheduler const&) = delete;
 
     auto add(I_TimedAction* action) -> void {
-        //eventQueue_ptr->push(action);
         timeTable_ptr->add(action);
     }
 
@@ -102,24 +107,23 @@ public:
 
     auto stop() -> void {
         watcher.isRunning = false;
+        std::cout << "[ Scheduler | STOPPED ]" << std::endl;
     }
 
-    auto restart() const -> void {
-        //        for (auto &entry : *timeTable_ptr) {
-        //            entry.second->restart();
-        //        }
-        std::cout << "Scheduler::restart() not implemented" << std::endl;
+    auto restart() -> void {
+        stop();
+        start();
     }
 
     [[nodiscard]] auto is_running() const -> bool {
         return watcher.isRunning;
     }
 
-    auto get_attribute(std::string_view const& key) -> std::string_view {
+    auto get_attribute(std::string const& key) -> std::string {
         return this->attributes[key];
     }
 
-    auto set_attribute(std::string_view const& key, std::string_view const& value) -> bool {
+    auto set_attribute(std::string const& key, std::string const& value) -> bool {
         if(key.empty() || value.empty()) {
             return false;
         }
@@ -131,26 +135,18 @@ public:
     /////////////////////////
 
     auto start_scheduler() -> void {
-        //        auto job_list = std::map<std::string, std::tm>{};
-        //
-        //        for(auto &action : *eventQueue_ptr) {
-        //            const auto time_points = action->get_execution_times();
-        //            for(auto &time_point : time_points) {
-        //                job_list.emplace(action->getName(), time_point);
-        //            }
-        //        }
-        std::cout << "Scheduler::start_scheduler() not implemented" << std::endl;
+        watcher.isRunning = true;
+        std::cout << "[ Scheduler | STARTED ]" << std::endl;
     }
 
     auto get_runtime_info() -> std::string {
         const auto time_t = std::chrono::system_clock::to_time_t(up_time);
         const auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        // run time in days + extra hours with minutes and seconds and
         const auto run_time = std::difftime(now, time_t);
-        const auto days = std::floor(run_time / 86'400);
-        const auto hours = std::floor((run_time - (days * 86'400)) / 3'600);
-        const auto minutes = std::floor((run_time - (days * 86'400) - (hours * 3'600)) / 60);
-        const auto seconds = std::floor((run_time - (days * 86'400) - (hours * 3'600) - (minutes * 60)));
+        const auto days = std::floor(run_time / DAY_SECONDS);
+        const auto hours = std::floor((run_time - (days * DAY_SECONDS)) / HOUR_SECONDS);
+        const auto minutes = std::floor((run_time - (days * DAY_SECONDS) - (hours * HOUR_SECONDS)) / MINUTE_SECONDS);
+        const auto seconds = std::floor((run_time - (days * DAY_SECONDS) - (hours * HOUR_SECONDS) - (minutes * MINUTE_SECONDS)));
 
         auto ss = new std::stringstream ();
         *ss << "[SCHEDULER RUNTIME INFO]" << std::endl;
@@ -162,7 +158,9 @@ public:
         *ss << "Scheduler is " << (watcher.isRunning ? "running" : "stopped") << "." << std::endl;
         *ss << "--------------------------------------------------------------------------------" << std::endl;
 
-        return ss->str();
+        std::string result = ss->str();
+        delete ss;
+        return result;
     }
 };
 
