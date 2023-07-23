6b74925face65219536c005699de3a0304906de5
CR <44606665+CodebyCR@users.noreply.github.com>
2023-07-23 17:08:17 +0200

    Add  - namespace  - get_attributes methode

--- Utilities/Environment.hpp
+++ Utilities/Environment.hpp
@@ -8,52 +8,56 @@
 #include <string_view>
 #include "ConfigJSON.hpp"
 
-constexpr std::string_view SCHEDULER = "Scheduler";
-constexpr std::string_view NOTIFICATION = "Notification";
-constexpr std::string_view LOG = "Log";
+namespace ENV {
+    constexpr std::string_view SCHEDULER = "Scheduler";
+    constexpr std::string_view NOTIFICATION = "Notification";
+    constexpr std::string_view LOG = "Log";
 
-/**
- * This class is a singleton.
- * It contains all the information about the environment.
- * It is used to configure the scheduler and the notification system.
- */
-class Environment {
-public:
-    Environment(const Environment&) = delete;
-    Environment& operator=(const Environment&) = delete;
-    Environment& operator=(Environment&&) = delete;
-    ~Environment() = default;
 
-    friend auto operator << (std::ostream& os, Environment& env) -> std::ostream& {
-        os << env.json.to_string();
-        return os;
-    }
-
-    ///// STATIC METHODS /////
-
-    static std::shared_ptr<Environment> getInstance(std::string_view input) {
-        if (!instance) {
-            instance = std::make_shared<Environment>( input);
+    /// return a environment variable or an empty string
+    static auto get_env_variable(std::string_view const env_variable) -> std::string {
+        auto env_value = std::getenv(env_variable.data());
+        if (!env_value) {
+            std::cout << env_variable << " not found" << std::endl;
+            return "";
         }
-        return instance;
+        return env_value;
     }
 
-//    static auto get_scheduler_attributes(Environment& env) -> std::optional<std::map<std::string, std::string>> {
-//        return instance
-//            ? env.json.get_optional_map(SCHEDULER)
-//            : std::nullopt;
-//    }
-//
-//    static auto get_notification_attributes(Environment& env) -> std::optional<std::map<std::string, std::string>> {
-//        return instance
-//            ? env.json.get_optional_map(NOTIFICATION)
-//            : std::nullopt;
-//    }
+    /**
+     * This class is a singleton.
+     * It contains all the information about the environment.
+     * It is used to configure the scheduler and the notification system.
+     */
+    class Environment {
+    public:
+        explicit Environment(std::string_view input) : json(input) {}
+        Environment(const Environment&) = delete;
+        Environment& operator=(const Environment&) = delete;
+        Environment& operator=(Environment&&) = delete;
+        ~Environment() = default;
 
-    explicit Environment(std::string_view input) : json(input) {}
+        friend auto operator<<(std::ostream& os, Environment& env) -> std::ostream& {
+            os << env.json.to_string();
+            return os;
+        }
 
-private:
-    static std::shared_ptr<Environment> instance;
-    ConfigJSON json;
+        auto get_attributes(std::string_view key) -> std::optional<std::map<std::string, std::string>> {
+            return json.get_optional_map(key);
+        }
 
-};
+        ///// STATIC METHODS /////
+
+        static std::shared_ptr<Environment> getInstance(std::string_view input) {
+            static std::shared_ptr<Environment> instance;
+            if(!instance) {
+                instance = std::make_shared<Environment>(input);
+            }
+            return instance;
+        }
+
+    private:
+        ConfigJSON json;
+    };
+
+}
