862a122247a0f6fde5ef7ac612f47612ab76d1d2
CR <44606665+CodebyCR@users.noreply.github.com>
2023-07-25 23:12:51 +0200

    Add ScheduledAction.hpp
    A new kind of Timed Action with a more lightweight and
    focused character

---
+++ TimedAction_Types/ScheduledAction.hpp
@@ -0,0 +1,67 @@
+//
+// Created by Christoph Rohde on 24.07.23.
+//
+#pragma once
+
+#include "../Interfaces/ActionCapsule.hpp"
+#include "../Interfaces/I_TimedAction.hpp"
+#include "../Notification/JobLog.hpp"
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+
+class ScheduledAction : public I_TimedAction {
+private:
+
+    /** Internal state */
+
+    bool isRunning {};
+    std::thread thread;
+
+
+
+public:
+    ScheduledAction(std::string_view const& name,
+                    I_ExecutionTimer* executionTimer,
+                    std::function<void(void)> const& action)
+        : I_TimedAction(name, executionTimer, action){}
+
+    ScheduledAction(ActionCapsule const& action)
+        : I_TimedAction(action.name, action.execution_timer, action.action){}
+
+    ~ScheduledAction() override = default;
+
+    auto run() -> void {
+        if (action) {
+            action();
+        }
+        else{
+            std::cout << "action is nullptr" << std::endl;
+        }
+    }
+
+    auto start() -> void override {
+        isRunning = true;
+        thread = std::thread(&ScheduledAction::run, this);
+        thread.detach();
+    }
+
+    auto stop() -> void override {
+        isRunning = false;
+        thread.join();
+    }
+
+    auto restart() -> void override {
+        stop();
+        start();
+    }
+
+    [[nodiscard]]
+    auto is_running() const -> bool override {
+        return isRunning;
+    }
+
+};
+
+
