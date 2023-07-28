11a6fde79dc16a6b52039e82e84a64ea2dbae2bf
CR <44606665+CodebyCR@users.noreply.github.com>
2023-07-28 14:25:51 +0200

    Add Dispatcher for configurable attribute access

--- Scheduler/Watcher.hpp
+++ Scheduler/Watcher.hpp
@@ -12,16 +12,27 @@
 
 class Watcher {
 private:
-    std::chrono::milliseconds watchInterval = std::chrono::milliseconds(1'000);
+    Dispatcher dispatcher;
+
+    std::chrono::milliseconds watchInterval = std::chrono::milliseconds(
+            Dispatcher::to_int_or_else(
+                    dispatcher.env_lookup(WATCHER, WATCH_INTERVAL),
+                    1'000));
 
 public:
-    constexpr static std::string_view watch_sleep       = "[ Watcher | SLEEP ] -> No active Watchables found.";
+    /// Map Key
+    constexpr static std::string_view WATCHER = "Watcher";
+
+    /// Sub Keys
+    constexpr static std::string_view WATCH_INTERVAL = "Watch Interval";
+
+    /// STATICS
+    constexpr static std::string_view watch_sleep = "[ Watcher | SLEEP ] -> No active Watchables found.";
 
     /// ! for std::format
     /// constexpr static std::string_view found_jobs = "[ Watcher | FOUND ] -> {0} found.";
     /// constexpr static std::string_view watcher_checking = "[ Watcher | CHECKING ] -> {0} for execution at {1}.";
     /// and so on...
-
 
     constexpr static unsigned long trigger_sleep_message = 300;
 
@@ -31,12 +42,11 @@ public:
 
     [[nodiscard]]
     auto open_watch_thread(std::vector<std::shared_ptr<Watchable>> const& watchables) const -> std::thread {
-
         return std::thread([&] {
             while(isRunning) {
                 std::this_thread::sleep_for(watchInterval);
 
-                bool inactive_watchables = std::ranges::all_of(watchables, [](auto & watchable) {
+                bool inactive_watchables = std::ranges::all_of(watchables, [](auto& watchable) {
                     return watchable->inactive();
                 });
 
@@ -50,18 +60,10 @@ public:
 
                 const std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
-                std::ranges::for_each(watchables, [&](auto & watchable) {
+                std::ranges::for_each(watchables, [&](auto& watchable) {
                     watchable->watch(now);
                 });
-
             }
         });
     }
-
-
-
-    auto set_attributes(std::map<std::string, std::string> attributes) -> void {
-        watchInterval = std::chrono::milliseconds(std::stoi(attributes["Watch Interval"]));
-    }
-
 };
