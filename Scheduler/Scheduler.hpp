f0ebbf12bdf84b210262d6992ea29998d79a5312
CR <44606665+CodebyCR@users.noreply.github.com>
2023-07-28 14:27:42 +0200

    Add Dispatcher for configurable attribute access
    - add constexpr map and sub key map
    - remove get / set attribute

--- Scheduler/Scheduler.hpp
+++ Scheduler/Scheduler.hpp
@@ -22,9 +22,98 @@
 
 
 class Scheduler {
+public:
+    virtual ~Scheduler() = default;
+
+    // Singleton
+    static auto get_instance() -> std::shared_ptr<Scheduler> {
+        static Scheduler instance;
+        return std::make_shared<Scheduler>(instance);
+    }
+
+    Scheduler(Scheduler const&) = default;
+    void operator=(Scheduler const&) = delete;
+
+    auto add(std::shared_ptr<I_TimedAction> const& action) -> void {
+        timeTable_ptr->add(action);
+    }
+
+    auto add(ActionCapsule const& action) -> void {
+        timeTable_ptr->add(std::make_shared<ScheduledAction>(action));
+    }
+
+    auto add(std::shared_ptr<Watchable> const& watchable) -> void {
+        if(!watchable) {
+            std::cout << "[ WatchableList | ERROR ] -> uninitialised Watchable." << std::endl;
+            return;
+        }
+
+        this->watchables.emplace_back(watchable);
+    }
+
+    /// Starts a new independent thread that watches the watchables.
+    auto start() -> void {
+        watcher.isRunning = true;
+        auto watcher_thread = watcher.open_watch_thread(watchables);
+        watcher_thread.detach();
+        std::cout << "[ Scheduler | STARTED ]" << std::endl;
+    }
+
+    auto stop() -> void {
+        watcher.isRunning = false;
+        std::cout << "[ Scheduler | STOPPED ]" << std::endl;
+    }
+
+    auto restart() -> void {
+        stop();
+        start();
+    }
+
+    [[nodiscard]]
+    auto is_running() const -> bool {
+        return watcher.isRunning;
+    }
+
+    /////////////////////////
+
+    auto get_runtime_info() -> std::string {
+        const auto time_t = std::chrono::system_clock::to_time_t(up_time);
+        const auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        const auto run_time = std::difftime(now, time_t);
+        const auto days = std::floor(run_time / DAY_SECONDS);
+        const auto hours = std::floor((run_time - (days * DAY_SECONDS)) / HOUR_SECONDS);
+        const auto minutes = std::floor((run_time - (days * DAY_SECONDS) - (hours * HOUR_SECONDS)) / MINUTE_SECONDS);
+        const auto seconds = std::floor((run_time - (days * DAY_SECONDS) - (hours * HOUR_SECONDS) - (minutes * MINUTE_SECONDS)));
+
+        auto ss = new std::stringstream();
+        *ss << "[ SCHEDULER | RUNTIME INFO ]" << std::endl;
+        *ss << "--------------------------------------------------------------------------------" << std::endl;
+        *ss << "Running since       " << std::ctime(&time_t) << std::endl;
+        *ss << "Current time:       " << std::ctime(&now) << std::endl;
+        *ss << "Run time:           " << days << " days, " << hours << " hours, "
+            << minutes << " minutes and " << seconds << " seconds." << std::endl;
+        *ss << "Scheduler contains  " << watchables.size() << " watchable(s)." << std::endl;
+        *ss << "Time Table has      " << timeTable_ptr->size() << " entries." << std::endl;
+        *ss << "Scheduler is        " << (watcher.isRunning ? "running" : "stopped") << "." << std::endl;
+        *ss << "--------------------------------------------------------------------------------" << std::endl;
+
+        std::string result = ss->str();
+        delete ss;
+        return result;
+    }
+
+protected:
+    /// Map Key
+    constexpr static std::string_view SCHEDULER = "Scheduler";
+
+    /// Configuration attributes
+    constexpr static std::string_view ENABLE_BLACKLISTS = "Enable Blacklists";
+
 private:
     std::chrono::system_clock::time_point up_time = {};
-    std::map<std::string, std::string> attributes;
+    Dispatcher dispatcher;
+    bool enable_blacklists = Dispatcher::to_bool_or_else(
+            dispatcher.env_lookup(SCHEDULER, ENABLE_BLACKLISTS), true);
     std::vector<std::shared_ptr<Watchable>> watchables;
     std::shared_ptr<TimeTable> timeTable_ptr;
     Watcher watcher;
@@ -36,9 +125,6 @@ private:
 
     Scheduler() {
         up_time = std::chrono::system_clock::now();
-        Dispatcher dispatcher;
-        attributes = dispatcher.get_scheduler_attributes();
-        watcher.set_attributes(dispatcher.get_watcher_attributes());
         timeTable_ptr = std::make_shared<TimeTable>();
         watchables.emplace_back(timeTable_ptr);
         // drop folder support
@@ -84,114 +170,4 @@ private:
             std::cout << "[ TimeTable | DROPPED ] -> '" << job->getName() << "'" << std::endl;
         });
     }
-
-
-public:
-    virtual ~Scheduler() = default;
-
-    // Singleton
-    static auto get_instance() -> std::shared_ptr<Scheduler> {
-        static Scheduler instance;
-        return std::make_shared<Scheduler>(instance);
-    }
-
-    Scheduler(Scheduler const&) = default;
-    void operator=(Scheduler const&) = delete;
-
-    auto add( std::shared_ptr<I_TimedAction> const& action) -> void {
-        timeTable_ptr->add(action);
-    }
-
-    auto add(ActionCapsule const& action) -> void {
-        timeTable_ptr->add(std::make_shared<ScheduledAction>(action));
-    }
-
-    auto add(std::shared_ptr<Watchable> const& watchable) -> void {
-        if(!watchable) {
-            std::cout << "[ WatchableList | ERROR ] -> uninitialised Watchable." << std::endl;
-            return;
-        }
-
-        this->watchables.emplace_back(watchable);
-    }
-
-    auto start() -> void {
-        /// new watcher thread & make it independent
-        auto watcher_thread = watcher.open_watch_thread(watchables);
-        watcher_thread.detach();
-    }
-
-    auto stop() -> void {
-        watcher.isRunning = false;
-        std::cout << "[ Scheduler | STOPPED ]" << std::endl;
-    }
-
-    auto restart() -> void {
-        stop();
-        start();
-    }
-
-    [[nodiscard]]
-    auto is_running() const -> bool {
-        return watcher.isRunning;
-    }
-
-    auto get_attribute(std::string const& key) -> std::string {
-        return this->attributes[key];
-    }
-
-    auto set_attribute(std::string const& key, std::string const& value) -> bool {
-        if(key.empty() || value.empty()) {
-            return false;
-        }
-
-        this->attributes[key] = value;
-        return true;
-    }
-
-    /////////////////////////
-
-    auto start_scheduler() -> void {
-        watcher.isRunning = true;
-        std::cout << "[ Scheduler | STARTED ]" << std::endl;
-    }
-
-    auto get_runtime_info() -> std::string {
-        const auto time_t = std::chrono::system_clock::to_time_t(up_time);
-        const auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        const auto run_time = std::difftime(now, time_t);
-        const auto days = std::floor(run_time / DAY_SECONDS);
-        const auto hours = std::floor((run_time - (days * DAY_SECONDS)) / HOUR_SECONDS);
-        const auto minutes = std::floor((run_time - (days * DAY_SECONDS) - (hours * HOUR_SECONDS)) / MINUTE_SECONDS);
-        const auto seconds = std::floor((run_time - (days * DAY_SECONDS) - (hours * HOUR_SECONDS) - (minutes * MINUTE_SECONDS)));
-
-        auto ss = new std::stringstream ();
-        *ss << "[ SCHEDULER | RUNTIME INFO ]" << std::endl;
-        *ss << "--------------------------------------------------------------------------------" << std::endl;
-        *ss << "Running since       " << std::ctime(&time_t) << std::endl;
-        *ss << "Current time:       " << std::ctime(&now) << std::endl;
-        *ss << "Run time:           " << days << " days, " << hours << " hours, "
-            << minutes << " minutes and " << seconds << " seconds." << std::endl;
-        *ss << "Scheduler contains  " << watchables.size() << " watchable(s)." << std::endl;
-        *ss << "Time Table has      " << timeTable_ptr->size() << " entries." << std::endl;
-        *ss << "Scheduler is        " << (watcher.isRunning ? "running" : "stopped") << "." << std::endl;
-        *ss << "--------------------------------------------------------------------------------" << std::endl;
-
-        std::string result = ss->str();
-        delete ss;
-        return result;
-    }
 };
-
-
-/// example Scheduler implementation
-
-// auto main() -> int {
-//     auto scheduler = Scheduler::get_instance();
-//     auto action = std::make_unique<TimedAction>("action", CronInterpreter::parse("*/5 * * * * *"));
-//     scheduler.add(action.get());
-//     scheduler.start();
-//     std::this_thread::sleep_for(std::chrono::seconds(10));
-//     scheduler.stop();
-//     return 0;
-// }
