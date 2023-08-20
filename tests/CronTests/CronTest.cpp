c28f57b852b955062ec6f1f0fa18cdd76d201e03
CR <44606665+CodebyCR@users.noreply.github.com>
2023-08-20 21:07:43 +0200

    Add Cron operator overloading '<<' test
    - test the '<<' - overloading
    - test out of date message

--- tests/CronTests/CronTest.cpp
+++ tests/CronTests/CronTest.cpp
@@ -8,20 +8,8 @@
 
 class CronTest : public ::testing::Test {
 protected:
-
     void SetUp() override {
         std::cout << "CronTest SetUp" << std::endl;
-        auto cron = Cron({
-                .second = "0",
-                .minute = "30",
-                .hour = "*/2",
-                .dayOfMonth = "15",
-                .month = "*/2",
-                .weekday = "*",
-                .year = "2023-2024"});
-
-        std::cout << cron << std::endl;
-
     }
 
     void TearDown() override {
@@ -29,10 +17,25 @@ protected:
     }
 
 
+public:
+    static auto getOutOfDateCron() -> Cron{
+        return Cron({
+                .second = "0",
+                .minute = "30",
+                .hour = "*/2",
+                .dayOfMonth = "15",
+                .month = "*/2",
+                .weekday = "*",
+                .year = "2023-2024"});
+    }
 };
 
-TEST_F(CronTest, test) {
-    std::cout << "CronTest test" << std::endl;
-    EXPECT_EQ(1, 1);
+TEST_F(CronTest, OPERATOR_TO_STRING) {
+    Cron cron = CronTest::getOutOfDateCron();
+    std::stringstream stream;
+    stream << cron;
+    std::string result = stream.str();
+
+    EXPECT_EQ(result, "No execution times contained. -> out-of-date expression");
 }
 
